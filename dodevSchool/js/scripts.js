bf143557919943f8451ca4ded741b637007b03ce
Loschii <bruno.loschi@outlook.com>
2024-04-15 14:03:50 -0300

    entregavel

--- dodevSchool/js/scripts.js
+++ dodevSchool/js/scripts.js
@@ -1,5 +1,64 @@
 ////////////////////////////////////////////////////////////////////////
-////////////////// FAÇA O SEU CÓDIGO AQUI \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+class Aluno{
+  Nome
+  Idade
+  Nota
+  constructor(nome, idade, nota){
+    this.Nome = nome
+    this.Idade = idade
+    this.Nota = nota
+  }
+}
+
+let arrayAlunos = []
+
+function CadastrarAluno(nome, idade, nota, array){
+  let objetoAluno = new Aluno(nome, idade, nota)
+  if(!array.some(x => x.Nome == nome)){
+    array.push(objetoAluno)
+  }
+  return objetoAluno
+}
+
+function OrdenarPorNota(array){
+  array.sort((a, b) => a.Nota - b.Nota)
+  return array
+}
+
+function OrdenarPorIdade(array){
+  array.sort((a, b) => a.Idade - b.Idade)
+  return array
+}
+
+function OrdenarPorNome(array){
+  array.sort((a, b) => {
+    const nomeA = a.Nome.toUpperCase();
+    const nomeB = b.Nome.toUpperCase();
+
+    if(nomeA < nomeB){
+      return -1;
+    } 
+    if(nomeA > nomeB){
+      return 1;
+    }
+    return 0;
+  });
+  return array
+}
+
+function CalcularMedia(array){
+  if(array.length === 0){
+    return 0;
+  }
+  let somaNotas = 0;
+
+  array.forEach((aluno) => {
+    somaNotas += Number(aluno.nota)
+  })
+
+  const media = somaNotas / array.length;
+  return media;
+}
 ////////////////////////////////////////////////////////////////////////
 
 class Aluno {
